c32125a36a45d51358e70a0efa54be834e500611
ShivamGS <sonawaneshivam01@gmail.com>
2026-02-17 11:39:54 -0700

    updates

--- components/sections/experience-section.tsx
+++ components/sections/experience-section.tsx
@@ -37,7 +37,7 @@ const experiences = [
     company: "LEIFII",
     companyUrl: "https://leifii.com/",
     location: "Pune, Maharashtra, India",
-    period: "July 2023 - June 2024",
+    period: "Jan 202 - June 2024",
     // type: "Full-time",
     achievements: [
       "Built full-stack web application for HR management using React, Node.js, Express, and MongoDB, streamlining workflows for 200+ employees and reducing manual effort by 40%.",
